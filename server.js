e9908874993071a48f512232fb2aca8805090859
Jacob Templeton <jwt3@stmarys-ca.edu>
2021-03-06 13:04:24 -0800

    register users proof of concept

--- server.js
+++ server.js
@@ -1,11 +1,65 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+	users: [
+	{
+		id: 123,
+		name: 'John Doe',
+		email: 'johndoe@gmail.com',
+		password: 'enter',
+		orders: 3,
+		dateJoined: new Date()
+	},
+	{
+		id: 124,
+		name: 'Jane Doe',
+		email: 'janedoe@gmail.com',
+		password: 'enter2',
+		orders: 5,
+		dateJoined: new Date()
+	}
+	]
+}
+
 app.get('/', (req, res)=> {
-	res.send('Response is working!')
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+		res.json('success!')
+	}else {
+		res.status(400).json('error with your login.')
+	}
+})
+
+app.post('/register', (req,res)=> {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: 125,
+		name: name,
+		email: email,
+		password: password,
+		orders: 0,
+		dateJoined: new Date()
+	})
+	res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000, ()=> {
 	console.log('App is working! Port 3000 looking good.');
 })
+
+/*
+Planned routes:
+/ --> res= Response is working!
+/signin --> POST user info, res=success/fail
+/register --> POST user data to db, res=user obj
+/profile/:userId --> GET user data, res=user  THIS WILL BE THE USER DASHBOARD TO SEE ORDERS IN PROCESS
+*/
